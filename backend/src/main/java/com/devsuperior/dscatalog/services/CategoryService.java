74a3dd3c5c0bbc2bc85add84d60056c1e664b97b
Leandro Ferreira <leandrofa1980@gmail.com>
2022-03-17 13:50:04 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,16 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true) // readOnly melhora a performace do banco de dados, evitando fazer um locking
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list =  repository.findAll();
+		
+		// Utilizando uma expressão lambda e tranformar para DTO
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		//Uma forma de transformar uma lista de Category para CategoryDTO
+		/*
+		 * List<CategoryDTO> listDto = new ArrayList<>(); for(Category cat : list) {
+		 * listDto.add(new CategoryDTO(cat)); }
+		 */
 	}
 }
